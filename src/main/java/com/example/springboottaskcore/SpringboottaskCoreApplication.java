0db30372205307d24f3bc55cebe191810cecb7b5
zhuzs212 <zhuzs212@163.com>
2022-05-10 14:36:32 +0800

    Initial commit

---
+++ src/main/java/com/example/springboottaskcore/SpringboottaskCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottaskcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottaskCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboottaskCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottaskcore/SpringboottaskCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottaskcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottaskCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
