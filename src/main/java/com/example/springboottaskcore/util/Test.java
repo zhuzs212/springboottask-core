9b04cd5426ce8ed5777845676c29838731dee7f8
zhuzs212 <zhuzs212@163.com>
2022-05-10 18:34:49 +0800

    A: 1.新增若干note file；

--- src/main/java/com/example/springboottaskcore/util/Test.java
+++ src/main/java/com/example/springboottaskcore/util/Test.java
@@ -1,8 +1,18 @@
 package com.example.springboottaskcore.util;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author zhu_zishuang
  * @date 2022/5/10 15:04
  */
+@Data
 public class Test {
+    /**
+     *
+     */
+    @NotBlank(message = "name不能为空！")
+    private String name;
 }
